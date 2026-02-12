f9a9cad8e423778331f349fc396c547b9c0ad6da
Yuragranovsky <yuragranovsky@gmail.com>
2026-02-12 20:16:13 +0200

    travelInfo tool

--- system_content.js
+++ system_content.js
@@ -3,6 +3,7 @@ You are helpful assistant with option for tool-routing.
 There are following available tools:
 - getWeather(city: string)
 - exchangeRate(currency_code_country_from: string, currency_code_country_to: string)
+- travelInfo(info: JSON string)
 YOUR JOB:
 - Decide whether a tool should be called
 - Your response for case when a tool should be called MUST contain ONLY SINGLE JSON in the right below JSON format. No markdown words, No comments, No explanations
@@ -14,6 +15,20 @@ YOUR JOB:
 INSTRUCTIONS FOR TOOLS CALL
 getWeather tool should be called when and only when the user's request contains weather-related terms (weather, wind, temperature, rain, humidity, sun) and a city name
 exchangeRate tool should be called when and only when the user's request contains currency rate and country/city from and country/city to. exchangeRate tool takes two arguments that are ISO 4217 currency code of country from and ISO 4217 currency code of country to
+travelInfo tool should be called when and only when the user's request contains travel city from and city to
+travelInfo tool takes only one argument that is JSON string with following structure :
+{
+"cityFrom": "city name from",
+"cityTo": "city name to",
+"countryFrom": "country name from",
+"countryTo": "country name to",
+"currencyCodeFrom": "ISO 4217 currency code from",
+"currencyCodeTo": "ISO 4217 currency code from",
+"currencyNameFrom": "currency name from",
+"currencyNameTo": "currency name to"
+}
+Your task for travelInfo provide required info as JSON with above structure. Don't put any comments, explanations,fences in that JSON structure Define country name based on city name. Define info about currency based on country name
+This JSON you should pass as travelInfo argument. Final reponse should be as {"tool": "travelInfo", "arguments": ["filled JSON string"]}. It must be JSON string
 VERY IMPORTANT NOTE: You must strictly follow the above instructions
 `
 export default INNER_SYSTEM_CONTENT
--- tools.js
+++ tools.js
@@ -5,7 +5,11 @@ async function getWeather(city) {
 async function exchangeRate(codeFrom, codeTo) {
     return exchangeRateService(codeFrom, codeTo)
 }
+async function travelInfo(info) {
+    return info;
+}
 export const TOOLS = {
     "getWeather": async (city) => await getWeather(city),
-    "exchangeRate": async (codeFrom, codeTo) => await exchangeRate(codeFrom, codeTo)
+    "exchangeRate": async (codeFrom, codeTo) => await exchangeRate(codeFrom, codeTo),
+    "travelInfo": async (info) => await travelInfo(info)
 }
